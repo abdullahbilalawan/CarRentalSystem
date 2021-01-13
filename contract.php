f3644a39770084a12e4cab404cbc39a7242f6998
Aliza Qamer <62201693+AlizaQamer0809@users.noreply.github.com>
2021-01-13 23:48:04 +0500

    Update contract.php

--- contract.php
+++ contract.php
@@ -2,9 +2,15 @@
    
 
     $ContractNumber=$_POST['ContractNumber'];
+    $CarNumber=$_POST['CarNumber'];
+    $CarModel=$_POST['CarModel'];
+    $BuyORRent=$_POST['BuyORRent'];
+    $CarColor=$_POST['CarColor'];
     $StartDate=$_POST['StartDate'];
     $LasttDate=$_POST['LasttDate'];
     $TotalPrice=$_POST['TotalPrice'];
+    
+
 
     $connect=new mysqli('localhost','root','','CarContract');
     if($connect->connect_error){
@@ -14,7 +20,7 @@
     else{
         
 
-        $query="INSERT INTO CONTRACT VALUES ('$ContractNumber','$StartDate','$LasttDate','$TotalPrice')";
+        $query="INSERT INTO CONTRACT VALUES ('$ContractNumber','$CarNumber','$CarModel','$BuyORRent','$CarColor','$StartDate','$LasttDate','$TotalPrice')";
         $data=mysqli_query($connect,$query);
 
 if ($data)
